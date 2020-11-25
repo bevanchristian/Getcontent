4fc9f703f3085c185f0de9ef6776975bc867ec48
bevanchristian <50088801+bevanchristian@users.noreply.github.com>
2020-11-25 23:22:32 +0700

    add untuk account

--- app/src/main/java/com/example/getcontent/home.kt
+++ app/src/main/java/com/example/getcontent/home.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_home.*
 
 class home : AppCompatActivity() {
@@ -19,10 +20,28 @@ class home : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         //setSupportActionBar(toolbar)
 
+
         initializeUI()
+        val user = FirebaseAuth.getInstance().currentUser
+        user?.let {
+            // Name, email address, and profile photo Url
+            val name = user.displayName
+            val email = user.email
+            val photoUrl = user.photoUrl
+
+            // Check if user's email is verified
+            val emailVerified = user.isEmailVerified
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            val uid = user.uid
+        }
 
 
     }
+
+
 
     private fun initializeUI() {
         sign_out_button.setOnClickListener {
