9cb144cc53ed16f2bcf0130e975841488cd834b8
bevanchristian <50088801+bevanchristian@users.noreply.github.com>
2020-11-25 17:52:26 +0700

    awalam

--- app/src/main/java/com/example/getcontent/MainActivity.kt
+++ app/src/main/java/com/example/getcontent/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.getcontent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Handler().postDelayed({
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+        },5000)
     }
 }
