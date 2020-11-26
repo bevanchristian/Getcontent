46747236b43ddc81090c481a60a931e76ff0136b
bevanchristian <50088801+bevanchristian@users.noreply.github.com>
2020-11-26 11:50:21 +0700

    account

--- app/src/main/java/com/example/getcontent/account.kt
+++ app/src/main/java/com/example/getcontent/account.kt
@@ -3,54 +3,77 @@ package com.example.getcontent
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_account.*
+import kotlin.properties.Delegates
 
 class account : AppCompatActivity() {
 
-lateinit var name:String
-    lateinit var email2:String
     companion object {
         fun getLaunchIntent(from: Context) = Intent(from, account::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
         }
     }
+
+    lateinit var name: String
+    lateinit var email2: String
+    var emailVerified by Delegates.notNull<Boolean>()
+    val user = FirebaseAuth.getInstance().currentUser
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
 
-        val user = FirebaseAuth.getInstance().currentUser
+
         user?.let {
             // Name, email address, and profile photo Url
             name = user.displayName.toString()
             email2 = user.email.toString()
-            val photoUrl = user.photoUrl
+            var photoUrl = user.photoUrl
+            Picasso.get().load(user.photoUrl.toString()).into(fotoprofil)
 
-            // Check if user's email is verified
-            val emailVerified = user.isEmailVerified
 
-            // The user's ID, unique to the Firebase project. Do NOT use this value to
-            // authenticate with your backend server, if you have one. Use
-            // FirebaseUser.getToken() instead.
-            val uid = user.uid
+
+
         }
-
         initprofil()
 
     }
-
-    fun initprofil(){
-        nama.text= Editable.Factory.getInstance().newEditable(name)
-        email.text=Editable.Factory.getInstance().newEditable(email2)
+    fun initprofil() {
+        nama.text = Editable.Factory.getInstance().newEditable(name)
+        email.text = Editable.Factory.getInstance().newEditable(email2)
+        if (user != null) {
+            hp.text=Editable.Factory.getInstance().newEditable(user.isEmailVerified.toString())
+        }
         //fotoprofil.setImageResource(Uri.parse(photo))
     }
+
+    override fun onResume() {
+        super.onResume()
+        updateuser.setOnClickListener {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(login.getLaunchIntent(this))
+        }
+
+
+    }
+
 
     override fun onStart() {
         super.onStart()
 
+
     }
 }
+
+
+
+
+
