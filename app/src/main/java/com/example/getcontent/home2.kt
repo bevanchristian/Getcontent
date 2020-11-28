de57fe65bd95e34402a20fb3205cfeeb36be678d
bevanchristian <50088801+bevanchristian@users.noreply.github.com>
2020-11-28 17:02:01 +0700

    bevan

--- app/src/main/java/com/example/getcontent/home2.kt
+++ app/src/main/java/com/example/getcontent/home2.kt
@@ -1,38 +1,36 @@
 package com.example.getcontent
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.getcontent.account
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.fragment_home2.view.*
 
 
 class home2 : Fragment() {
+lateinit var aa:View
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        initializeUI()
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home2, container, false)
+         aa= inflater.inflate(R.layout.fragment_home2, container, false)
+        initializeUI()
+        return aa
+
     }
 
     private fun initializeUI() {
-        sign_out_button.setOnClickListener {
+        aa.sign_out_button.setOnClickListener {
             logout()
         }
 
-        account.setOnClickListener {
+        aa.account.setOnClickListener {
             account()
         }
     }
