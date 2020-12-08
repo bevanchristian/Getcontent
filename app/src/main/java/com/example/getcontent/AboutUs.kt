0bbf414bf07d3f87723aa291928ebfee18d9fe2f
nimaduddin <49326968+nimaduddin@users.noreply.github.com>
2020-12-08 21:39:31 +0700

    latian ngelink dari aboutus ke account

--- app/src/main/java/com/example/getcontent/AboutUs.kt
+++ app/src/main/java/com/example/getcontent/AboutUs.kt
@@ -1,18 +1,34 @@
 package com.example.getcontent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_about_us.*
 
-class AboutUs : AppCompatActivity() {
+class AboutUs : AppCompatActivity(), View.OnClickListener {
 
 
-
+    private lateinit var var_btn_tes : Button //by aziz
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_us)
-
+        var_btn_tes = findViewById(R.id.btn_tes) //by aziz
+        var_btn_tes.setOnClickListener(this) //by aziz
 
     }
+
+    //by aziz #########
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btn_tes ->{
+                val var_otw_acc2 = Intent(this@AboutUs, account2::class.java)
+                startActivity(var_otw_acc2)
+            }
+        }
+    }
+    //#################
+
 }
