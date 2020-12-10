c407106a610a3a24ad204a39e2640b9dadc1e80a
bevanchristian <50088801+bevanchristian@users.noreply.github.com>
2020-12-10 09:02:13 +0700

    seeekkkk

--- app/src/main/java/com/example/getcontent/Howitwork.kt
+++ app/src/main/java/com/example/getcontent/Howitwork.kt
@@ -10,11 +10,7 @@ class Howitwork: AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.how_it)
-        var_btn_tes = findViewById(R.id.btn_tes) //by aziz
-        var_btn_tes.setOnClickListener{
-            val var_otw_acc2 = Intent(this@AboutUs, account2::class.java)
-            startActivity(var_otw_acc2)
+
         } //by aziz
 
     }
-}
