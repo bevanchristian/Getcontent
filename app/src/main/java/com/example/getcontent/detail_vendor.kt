6a4799ce969aa8d272f76e66ec98494fb71a8dbc
bevanchristian <50088801+bevanchristian@users.noreply.github.com>
2020-12-10 17:14:07 +0700

    mindah ke detail vendor

--- app/src/main/java/com/example/getcontent/detail_vendor.kt
+++ app/src/main/java/com/example/getcontent/detail_vendor.kt
@@ -1,21 +1,27 @@
 package com.example.getcontent
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detail_vendor.*
 
 
 class detail_vendor : AppCompatActivity() {
-
+    private lateinit var id:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_vendor)
+
+
+
     }
 
     override fun onResume() {
+
         super.onResume()
+        val data = intent.extras!!.getInt("idvendorrr")
+        vendor.text=data.toString()
         var gambar = mutableListOf<ImageView>(gold,silver,photoshoot,videoshoot)
         for(x in gambar){
             x.setOnClickListener {
