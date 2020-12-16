e6c72028505bd5740cdaaddae7c40f5f92b1e88a
nimaduddin <49326968+nimaduddin@users.noreply.github.com>
2020-12-16 21:33:39 +0700

    zoom image + fixed image = gabisa ssemua huh :(

--- app/src/main/java/com/example/getcontent/detailservice.kt
+++ app/src/main/java/com/example/getcontent/detailservice.kt
@@ -106,3 +106,32 @@ class detailservice : AppCompatActivity() {
     }
     
 }
+
+//bikin fixed image
+//public class ScrollingActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_detailservice);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//
+//        if (id==R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//}
