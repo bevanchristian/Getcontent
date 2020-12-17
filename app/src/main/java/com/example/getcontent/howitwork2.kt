57f587826a490c0f8576c04224aa657486177807
alamsyah1810 <37900853+alamsyah1810@users.noreply.github.com>
2020-12-17 10:55:18 +0800

    jhnj

---
+++ app/src/main/java/com/example/getcontent/howitwork2.kt
@@ -0,0 +1,11 @@
+package com.example.getcontent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class howitwork2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_howitwork2)
+    }
+}
