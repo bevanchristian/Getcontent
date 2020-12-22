fda33f66179ee3f7042e3501aaecfea57459e341
alamsyah1810 <37900853+alamsyah1810@users.noreply.github.com>
2020-12-23 05:55:20 +0800

    ngentot

--- app/src/main/java/com/example/getcontent/home2.kt
+++ app/src/main/java/com/example/getcontent/home2.kt
@@ -3,24 +3,25 @@ package com.example.getcontent
 //package com.kharismarizqii.belajarintent
 
 import android.content.Intent
+import android.graphics.*
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.HorizontalScrollView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.getcontent.database.AppDatabase
 import com.example.getcontent.recycleadapter.*
 import com.google.firebase.auth.FirebaseAuth
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.Transformation
+import jp.wasabeef.picasso.transformations.RoundedCornersTransformation
 import kotlinx.android.synthetic.main.activity_about_us.view.*
+import kotlinx.android.synthetic.main.fragment_account2.view.*
 import kotlinx.android.synthetic.main.fragment_home2.view.*
+
 
 // CODE BY AZIZ, LINK HOMEPAGE TO ACCOUNT2 EXPERIMENT
 //class home2 : AppCompatActivity(), View.OnclickListener, View.OnClickListener {
@@ -35,17 +36,42 @@ lateinit var aa:View
 
 
     private var projek= mutableListOf<project>()
+
+    val radius = 5
+    val margin = 5
+    val transformation: Transformation = RoundedCornersTransformation(radius, margin)
     //private var namadesign= mutableListOf<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         aa= inflater.inflate(R.layout.fragment_home2, container, false)
+        var about_us="https://drive.google.com/file/d/1esLH7PDu_fBjuGXbs3j64iYt4quCo4Vc/view?usp=sharing"
+        val p: Array<String> = about_us.split("/").toTypedArray()
+        val imageLink = "https://drive.google.com/uc?export=download&id=" + p[5]
+        Picasso.get().load(imageLink.toString()).transform(transformation).into(aa.tv_aboutus)
+        var how_it="https://drive.google.com/file/d/1bNNGv_GWqBTkL5Y2h0h6oLUNP7sGtVxT/view?usp=sharing"
+        val x: Array<String> = how_it.split("/").toTypedArray()
+        val gambar_howit = "https://drive.google.com/uc?export=download&id=" + x[5]
+        Picasso.get().load(gambar_howit.toString()).transform(transformation).into(aa.tv_howit)
         /*layout manager banner,agency*/
-        aa.rv_agency.layoutManager=LinearLayoutManager(this.requireActivity(),LinearLayoutManager.HORIZONTAL,false)
-        aa.rv_banner.layoutManager=LinearLayoutManager(this.requireActivity(),LinearLayoutManager.HORIZONTAL,false)
-        aa.rv_design.layoutManager=LinearLayoutManager(this.requireActivity(),LinearLayoutManager.HORIZONTAL,false)
+        aa.rv_agency.layoutManager=LinearLayoutManager(
+            this.requireActivity(),
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        aa.rv_banner.layoutManager=LinearLayoutManager(
+            this.requireActivity(),
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        aa.rv_design.layoutManager=LinearLayoutManager(
+            this.requireActivity(),
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
         /* di tarik dari sql dan api*/
 
         return aa
@@ -53,13 +79,40 @@ lateinit var aa:View
 
     }
 
+//    class GakNgotak : Transformation {
+//        override fun transform(source: Bitmap): Bitmap {
+//            val output = Bitmap.createBitmap(
+//                source.width, source
+//                    .height, Bitmap.Config.ARGB_8888
+//            )
+//            val canvas = Canvas(output)
+//            val color = -0xbdbdbe
+//            val paint = Paint()
+//            val rect = Rect(0, 0, source.width, source.height)
+//            val rectF = RectF(rect)
+//            val roundPx = 50f
+//            paint.isAntiAlias = true
+//            canvas.drawARGB(0, 0, 0, 0)
+//            paint.color = color
+//            canvas.drawRoundRect(rectF, roundPx, roundPx, paint)
+//            paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+//            canvas.drawBitmap(source, rect, rect, paint)
+//            source.recycle()
+//            return output
+//        }
+//
+//        override fun key(): String {
+//            return "RoundImage"
+//        }
+//    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         posttolist()
         /*dipasang*/
         aa.rv_banner.adapter=banneradapter(banner)
 
-        aa.rv_agency.adapter=vendoradapter(vendor )
+        aa.rv_agency.adapter=vendoradapter(vendor)
 
 
 
@@ -115,14 +168,14 @@ lateinit var aa:View
 
 
 
-    private fun addtolist(image:String){
+    private fun addtolist(image: String){
         banner.add(image)
     }
 
     private fun addtolistvendor(vendor: Vendor){
         this.vendor.add(vendor)
     }
-    private fun addtolistdesign(project:project){
+    private fun addtolistdesign(project: project){
         this.projek.add(project)
     }
 
@@ -161,8 +214,11 @@ lateinit var aa:View
 
                             //ini isi activity intent
                            // Toast.makeText(this@home2.requireContext(),nmvendor, Toast.LENGTH_SHORT).show()
-                            var pindah=Intent(this@home2.requireContext(),detail_vendor::class.java)
-                            pindah.putExtra("idvendorrr",idvendor)
+                            var pindah=Intent(
+                                this@home2.requireContext(),
+                                detail_vendor::class.java
+                            )
+                            pindah.putExtra("idvendorrr", idvendor)
                             startActivity(pindah)
 
                         }
@@ -187,11 +243,18 @@ lateinit var aa:View
                         this.onItemClickListener={
 
                             //ini isi activity intent
-                            Toast.makeText(this@home2.requireContext(),idpaketvendor.toString(), Toast.LENGTH_SHORT).show()
-                            var pindah=Intent(this@home2.requireContext(),detailservice::class.java)
-                            pindah.putExtra("idpaket",idpaketvendor.toInt())
-                            pindah.putExtra("namavendor",namavendor)
-                            pindah.putExtra("idvendor",idvendordesign)
+                            Toast.makeText(
+                                this@home2.requireContext(),
+                                idpaketvendor.toString(),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            var pindah=Intent(
+                                this@home2.requireContext(),
+                                detailservice::class.java
+                            )
+                            pindah.putExtra("idpaket", idpaketvendor.toInt())
+                            pindah.putExtra("namavendor", namavendor)
+                            pindah.putExtra("idvendor", idvendordesign)
                             startActivity(pindah)
 
 
