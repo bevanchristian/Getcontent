e5b2f1ba5aaa7dc375baa530a562fdba88f4e5b1
alamsyah1810 <37900853+alamsyah1810@users.noreply.github.com>
2020-12-26 18:26:07 +0800

    Update portofoliovendoradapter.kt

--- app/src/main/java/com/example/getcontent/recycleadapter/portofoliovendoradapter.kt
+++ app/src/main/java/com/example/getcontent/recycleadapter/portofoliovendoradapter.kt
@@ -5,23 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.getcontent.R
 import com.squareup.picasso.Picasso
 import com.squareup.picasso.Transformation
 import jp.wasabeef.picasso.transformations.MaskTransformation
 
-class portofoliovendoradapter (private var image:List<String>):
+class portofoliovendoradapter (private var image:List<detailprojek>):
     RecyclerView.Adapter<portofoliovendoradapter.ViewHolder>(){
 
     inner class ViewHolder(itemview: View): RecyclerView.ViewHolder(itemview){
         val itemimage: ImageView =itemview.findViewById(R.id.vendor_post1)
+        val nama: TextView =itemview.findViewById(R.id.namaproject)
+        val hh=itemview.context
         // var tulisan:TextView=itemview.findViewById(R.id.cek)
 
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): portofoliovendoradapter.ViewHolder {
         val v= LayoutInflater.from(parent.context).inflate(R.layout.portofoliodetailvendor,parent,false)
         return ViewHolder(v)
+
 
     }
 
@@ -31,8 +35,13 @@ class portofoliovendoradapter (private var image:List<String>):
     }
 
     override fun onBindViewHolder(holder: portofoliovendoradapter.ViewHolder, position: Int) {
+        val transformation2: Transformation = MaskTransformation(holder.hh, R.drawable.rounded_convers_transformation)
+        Picasso.get().load(image[position].fotoprojekadapter).transform(transformation2).into(holder.itemimage)
+        holder.nama.text=image[position].namaprojekadapter
+        holder.itemView.setOnClickListener {
+            image[position].onItemClickListener?.invoke()
+        }
 
-        Picasso.get().load(image[position]).into(holder.itemimage)
         Log.d("bener2","agency sudah ok")
 
     }
