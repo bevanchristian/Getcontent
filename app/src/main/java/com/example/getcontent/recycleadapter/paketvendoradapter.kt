d66aeee0b821ac580de868e2b753e1147a1f7bad
alamsyah1810 <37900853+alamsyah1810@users.noreply.github.com>
2020-12-28 20:47:46 +0800

    jgjjfj

--- app/src/main/java/com/example/getcontent/recycleadapter/paketvendoradapter.kt
+++ app/src/main/java/com/example/getcontent/recycleadapter/paketvendoradapter.kt
@@ -9,12 +9,15 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.getcontent.R
 import com.squareup.picasso.Picasso
+import com.squareup.picasso.Transformation
+import jp.wasabeef.picasso.transformations.MaskTransformation
 
 class paketvendoradapter (private var Paket:List<Paket>):
     RecyclerView.Adapter<paketvendoradapter.ViewHolder>(){
 
     inner class ViewHolder(itemview: View): RecyclerView.ViewHolder(itemview){
         val itemimage: ImageView =itemview.findViewById(R.id.paket)
+        val hh=itemview.context
         // var tulisan:TextView=itemview.findViewById(R.id.cek)
 
     }
@@ -29,7 +32,9 @@ class paketvendoradapter (private var Paket:List<Paket>):
     }
 
     override fun onBindViewHolder(holder: paketvendoradapter.ViewHolder, position: Int) {
-        Picasso.get().load(Paket[position].gambar).into(holder.itemimage)
+        val transformation2: Transformation = MaskTransformation(holder.hh, R.drawable.rounded_convers_transformation)
+        Picasso.get().load(Paket[position].gambar).transform(transformation2).into(holder.itemimage)
+//        Picasso.get().load(Paket[position].gambar).into(holder.itemimage)
         holder.itemView.setOnClickListener {
             Paket[position].onItemClickListener?.invoke()
         }
